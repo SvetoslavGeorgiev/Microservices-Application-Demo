73b78650987ecc599f33039411f15ad27c49f13f
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-07-22 22:31:33 +0100

    Configurating inMemory DB and seeding it

---
+++ PlatformService/PlatformService/Data/IPlatformRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace PlatformService.Data
+{
+    using PlatformService.Models;
+
+    public interface IPlatformRepository
+    {
+        Task<bool> SaveChanges();
+
+        Task<IEnumerable<Platform>> GetAllPlatforms();
+
+        Task<Platform> GetPlatformById(int id);
+
+        Task CreatePlatform(Platform platform);
+    }
+}
---
+++ PlatformService/PlatformService/Data/InMemoryDbPreparation.cs
@@ -0,0 +1,35 @@
+﻿namespace PlatformService.Data
+{
+    using PlatformService.Models;
+
+    public static class InMemoryDbPreparation
+    {
+        public static async Task PrepPopulation(WebApplication app)
+        {
+            using (var serviceScope = app.Services.CreateScope())
+            {
+                await SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static async Task SeedData(AppDbContext dbContext)
+        {
+            if (!dbContext.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data");
+
+                dbContext.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetis", Publisher = "Cloud Native Computing Foundation", Cost = "Free" });
+
+
+                await dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        }
+    }
+}
