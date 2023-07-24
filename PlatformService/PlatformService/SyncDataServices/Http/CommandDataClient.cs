e0078f3145bf97b97e5a2325c08f3c3b5438adb5
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-07-24 12:14:58 +0100

    implementing sync data services

---
+++ PlatformService/PlatformService/SyncDataServices/Http/CommandDataClient.cs
@@ -0,0 +1,38 @@
+﻿namespace PlatformService.SyncDataServices.Http
+{
+    using PlatformService.Dtos;
+    using System.Text;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+
+    public class CommandDataClient : ICommandDataClient
+    {
+
+        private readonly HttpClient httpClient;
+        private readonly IConfiguration configuration;
+
+        public CommandDataClient(HttpClient _httpClient, IConfiguration _configuration)
+        {
+            httpClient = _httpClient;
+            configuration = _configuration;
+        }
+        public async Task SendPlatformToCommand(PlatformReadDto platformReadDto)
+        {
+            var httpContent = new StringContent(
+                    JsonSerializer.Serialize(platformReadDto),
+                    Encoding.UTF8,
+                    "application/json");
+
+            var response = await httpClient.PostAsync($"{configuration["CommandService"]}", httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync POST to CommandService was OK!");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync POST to CommandService Failed!");
+            }
+        }
+    }
+}
---
+++ PlatformService/PlatformService/SyncDataServices/Http/ICommandDataClient.cs
@@ -0,0 +1,9 @@
+﻿namespace PlatformService.SyncDataServices.Http
+{
+    using PlatformService.Dtos;
+
+    public interface ICommandDataClient
+    {
+        Task SendPlatformToCommand(PlatformReadDto platformReadDto);
+    }
+}
