550760df80599c4b271323af7efe520ed76bbf7f
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-07-24 12:06:41 +0100

    Added Command services

---
+++ PlatformService/CommandService/Controllers/CommandController.cs
@@ -0,0 +1,11 @@
+﻿namespace CommandService.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+    }
+}
---
+++ PlatformService/CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿namespace CommandService.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test for the platforms Controller");
+        }
+    }
+}
