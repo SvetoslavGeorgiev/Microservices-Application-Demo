40c0ac9abe80c8aac8c0c46dea0b2a9a9b8ffcd6
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-07-24 12:15:20 +0100

    update platform services

--- PlatformService/PlatformService/Controllers/PlatformController.cs
+++ PlatformService/PlatformService/Controllers/PlatformController.cs
@@ -4,6 +4,8 @@
     using Microsoft.AspNetCore.Mvc;
     using PlatformService.Data;
     using PlatformService.Dtos;
+    using PlatformService.Models;
+    using PlatformService.SyncDataServices.Http;
 
     [Route("api/[controller]")]
     [ApiController]
@@ -11,15 +13,21 @@
     {
         private readonly IPlatformRepository repository;
         private readonly IMapper mapper;
+        private readonly ICommandDataClient commandDataClient;
 
-        public PlatformController(IPlatformRepository _repository, IMapper _mapper)
+        public PlatformController(
+            IPlatformRepository _repository,
+            IMapper _mapper,
+            ICommandDataClient _commandDataClient)
         {
             repository = _repository;
             mapper = _mapper;
+            commandDataClient = _commandDataClient;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<PlatformCreateDto>>> GetAllPlatforms() 
+        [Route("All")]
+        public async Task<IActionResult> GetAllPlatforms() 
         {
             Console.WriteLine("--> Getting Platforms....");
 
@@ -28,6 +36,49 @@
             var model = mapper.Map<IEnumerable<PlatformReadDto>>(platformItems);
 
             return Ok(model);
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public async Task<IActionResult> GetPlatformById([FromRoute]int id)
+        {
+            var platformItems = await repository.GetPlatformById(id);
+
+            var model = mapper.Map<PlatformReadDto>(platformItems);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = mapper.Map<Platform>(platformCreateDto);
+
+            //this is to check if going to return correct Id if I swap the database from inMemory to SQL
+
+            //var platdormId = await repository.CreatePlatform(platformModel);
+            await repository.CreatePlatform(platformModel);
+
+            await repository.SaveChanges();
+            //var platformItems = await repository.GetPlatformById(platdormId);
+            //var model = mapper.Map<PlatformReadDto>(platformItems);
+
+            var platdormReadDto = mapper.Map<PlatformReadDto>(platformModel);
+
+            try
+            {
+                await commandDataClient.SendPlatformToCommand(platdormReadDto);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"--> Cloud not send synchronously: {e.Message}");
+            }
+
+            return CreatedAtAction(nameof(GetPlatformById), new PlatformReadDto { Id = platdormReadDto.Id }, platdormReadDto);
         }
     }
 }
